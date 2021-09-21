2a80c9933c616f366587e5957de64ea8c55c3915
MZ6072 <romain.coville@engie.com>
2021-09-22 00:57:36 +0200

    requirements

--- global_variables.py
+++ global_variables.py
@@ -1,5 +1,5 @@
 URL = 'https://es.ign.com'
-DATASET = 'https://github.com/RomainCoville/ign_scraper/blob/781720208de94246f045dac0541a882c9aeb773b/test.csv'
+DATASET = '/app/ign_scraper'
 
 month_es = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto',
              'septiembre', 'octubre', 'noviembre', 'diciembre']
